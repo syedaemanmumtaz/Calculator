3bf547e9879bafdf47fa5dfbbcb8bc168b4b4fa3
syedaemanmumtaz <syedaeman027@gmail.com>
2024-08-08 20:48:56 +0500

    calculater.commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let input = document.getElementById("inputbox");
 let button = document.querySelectorAll("button");
 
-let string = "";
+let string = " ";
 let arr = Array.from(button);
 arr.forEach(button =>{
     button.addEventListener('click',(e)=>{
@@ -13,12 +13,10 @@ arr.forEach(button =>{
             string = "";
             input.value = string;
         }
-
         else if(e.target.innerHTML === "DEL"){
             string = string.substring(0, string.length - 1);
             input.value = string;
         }
-
         else{ string += e.target.innerHTML;
             input.value = string;
         }
